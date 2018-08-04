e93c69fe9c59fe68671ff2964419a96395763c1e
Suraj <surajonmail@gmail.com>
2018-08-04 01:30:19 -0500

    Match simulation visual update
    Match simulation visual update in GUI. League Standings in correct order.

--- src/LaLigaMain.java
+++ src/LaLigaMain.java
@@ -25,7 +25,7 @@ public class LaLigaMain
 		System.out.println("                   Teams                         ");
 		System.out.println("-------------------------------------------------");
 		
-		if(numTeams > minTeams)
+		if(numTeams >= minTeams)
 		{
 			for(int i=0; i<numTeams; ++i)
 			{
@@ -46,8 +46,7 @@ public class LaLigaMain
 		
 		int allMatches[][] = new int[2][2];
 		allMatches = leagueGames.matchingTeam(allTeams);
-		
-		
+				
 		ArrayList<String> teama = allTeams[0].getTeamGames();
 		// Printing elements one by one
 		System.out.println(" ");
@@ -56,73 +55,75 @@ public class LaLigaMain
         for (int i=0; i<teama.size(); i++)
             System.out.println(teama.get(i));
         
-        ArrayList<String> teamb = allTeams[1].getTeamGames();
-		// Printing elements one by one
-		System.out.println(" ");
-		System.out.println("Game schedule for Real Madrid ");
-		System.out.println("---------------------------- ");
-        for (int i=0; i<teamb.size(); i++)
-            System.out.println(teamb.get(i));
-        
         int[][] leagueTable = null;// initialize league table
         int[][] leagueStanding = null; // initialize league standings
         Object[] menuOptions = {"Teams","Match Schedule","Match Simulation","Standings","Exit"};
         int answer;
         do {
-        	answer = JOptionPane.showOptionDialog(null, "message", "Your Options", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOptions, null);
-		
-		switch (answer)
-		{
-		case 0: // team info
-			System.out.println("No.  Teams             Played      Wins        Draws       Losses      Points ");
-			System.out.println("------------------------------------------------------------------------------");
-			for(int i=0; i<allTeams.length; i++)
-			{
-				System.out.format("%2d%2s%-17s%6d%12d%12d%12d%12d\n", (i+1) , "  " ,
-						allTeams[i].getTeamName(),
-						allTeams[i].getGamesPlayed(),allTeams[i].getGamesWon(),
-						allTeams[i].getGamesDrawn(),allTeams[i].getGamesLost(),
-						allTeams[i].getLeaguePoints());
-			}
-			break;
-			
-		case 1: // match schedule
-			System.out.println("                   League Games                         ");
-			System.out.println("--------------------------------------------------------");
-			for(int i=0; i<allMatches.length; i++)
-			{
-				System.out.println("Matchday" + (i+1) + ": "+ allTeams[allMatches[i][0]].getTeamName() + " vs " + allTeams[allMatches[i][1]].getTeamName());
-			}
-			break;
-			
-		case 2://match simulation
-			MatchSimulation simulation = new MatchSimulation();
-	        leagueTable = simulation.start(allTeams, allMatches);
-			break;
-			
-		case 3: // league standings
-			MatchSimulation simulation1 = new MatchSimulation();
-			leagueStanding = simulation1.leagueStanding(leagueTable);
-			
-			System.out.println("Rank  Teams             Played      Wins        Draws       Losses      Points ");
-			System.out.println("------------------------------------------------------------------------------");
-			for(int i=0; i<allTeams.length; i++)
-			{
-				System.out.format("%4d%2s%-17s%6d%12d%12d%12d%12d\n", (i+1) , "  " ,
-						allTeams[leagueStanding[i][0]].getTeamName(),
-						allTeams[leagueStanding[i][0]].getGamesPlayed(),
-						allTeams[leagueStanding[i][0]].getGamesWon(),
-						allTeams[leagueStanding[i][0]].getGamesDrawn(),
-						allTeams[leagueStanding[i][0]].getGamesLost(),
-						allTeams[leagueStanding[i][0]].getLeaguePoints());
-			}
-			
-			break;
-			
-		default: 
-			System.out.println("all match schedule");
-			break;
-		}
+        	
+        	answer = JOptionPane.showOptionDialog(null, "Pick an option", "Your Options", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOptions, null);
+      
+        	{
+				switch (answer)
+				{
+				case 0: // team info
+					System.out.println("No.  Teams             Played      Wins        Draws       Losses      Points ");
+					System.out.println("------------------------------------------------------------------------------");
+					for(int i=0; i<allTeams.length; i++)
+					{
+						System.out.format("%2d%2s%-17s%6d%12d%12d%12d%12d\n", (i+1) , "  " ,
+								allTeams[i].getTeamName(),
+								allTeams[i].getGamesPlayed(),allTeams[i].getGamesWon(),
+								allTeams[i].getGamesDrawn(),allTeams[i].getGamesLost(),
+								allTeams[i].getLeaguePoints());
+					}
+					break;
+					
+				case 1: // match schedule
+					System.out.println("                   League Games                         ");
+					System.out.println("--------------------------------------------------------");
+					for(int i=0; i<allMatches.length; i++)
+					{
+						System.out.println("Matchday" + (i+1) + ": "+ allTeams[allMatches[i][0]].getTeamName() + " vs " + allTeams[allMatches[i][1]].getTeamName());
+					}
+					break;
+					
+				case 2://match simulation
+					MatchSimulation simulation = new MatchSimulation();
+			        leagueTable = simulation.start(allTeams, allMatches);
+					break;
+					
+				case 3: // league standings
+					
+					if(leagueTable!=null)
+					{
+						MatchSimulation simulation1 = new MatchSimulation();
+						leagueStanding = simulation1.leagueStanding(leagueTable);
+												System.out.println("Rank  Teams             Played      Wins        Draws       Losses      Points ");
+						System.out.println("-------------------------------------------------------------------------------");
+						for(int i=0; i<allTeams.length; i++)
+						{
+							System.out.format("%4d%2s%-17s%4d%12d%12d%12d%12d\n", (i+1) , "  " ,
+									allTeams[leagueStanding[i][0]].getTeamName(),
+									allTeams[leagueStanding[i][0]].getGamesPlayed(),
+									allTeams[leagueStanding[i][0]].getGamesWon(),
+									allTeams[leagueStanding[i][0]].getGamesDrawn(),
+									allTeams[leagueStanding[i][0]].getGamesLost(),
+									allTeams[leagueStanding[i][0]].getLeaguePoints());
+						}
+					}
+					else
+					{
+						System.out.println("First run match simulation");
+					}
+					
+					
+					break;
+					
+				default: 
+					break;
+				}
+        	}
 		
         } while(answer !=4);
         
--- src/MatchSimulation.java
+++ src/MatchSimulation.java
@@ -1,8 +1,17 @@
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 public class MatchSimulation {
 
@@ -13,11 +22,73 @@ public class MatchSimulation {
 		// initialize league table : team no., played, won, drawn, lost, points
 		int[][] leagueTable = new int[allTeams.length][6];
 		
+		// setup output window
+		int rows = allMatches.length;
+	    int cols = 4;
+	    
+	    int frameHeight = (rows*35);
+	    int frameWidth = (cols * 100);
+	    
+	    if(frameHeight<100)
+	    	frameHeight=100;
+
+	    JFrame frame = new JFrame("Match Simulation");
+
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(frameWidth,frameHeight);
+	    
+
+	    JPanel pane = new JPanel(new GridBagLayout());
+
+	    GridBagConstraints c = new GridBagConstraints();
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.ipady = 10;
+	    c.ipadx = 10;
+	    
+	    for(int i=0; i<rows; i++)
+	    {
+	    	JLabel label1 = new JLabel("Matchday "+ (i+1));
+	    	c.gridx = 0;
+	    	c.gridy = i;
+	    	label1.setHorizontalAlignment(SwingConstants.LEFT);
+	    	pane.add(label1,c);
+	    	
+	    	label1 = new JLabel(allTeams[allMatches[i][0]].getTeamName());
+	    	c.gridx = 1;
+	    	c.gridy = i;
+	    	label1.setHorizontalAlignment(SwingConstants.RIGHT);
+	    	pane.add(label1,c);
+	    	
+	    	label1 = new JLabel("  vs  ");
+	    	c.gridx = 2;
+	    	c.gridy = i;
+	    	label1.setHorizontalAlignment(SwingConstants.CENTER);
+	    	pane.add(label1,c);
+	    	
+	    	label1 = new JLabel(allTeams[allMatches[i][1]].getTeamName());
+	    	c.gridx = 3;
+	    	c.gridy = i;
+	    	label1.setHorizontalAlignment(SwingConstants.LEFT);
+	    	pane.add(label1,c);	    	
+	    }
+	    
+	    
+	    frame.getContentPane().add(pane);
+	    frame.setVisible(true);
+	    	    
+		// start simulation
 		for(int i=0; i<allMatches.length; i++)
 		{
-			System.out.println("\n Matchday "+(i+1)+" :  "+ allTeams[allMatches[i][0]].getTeamName() + " vs " + allTeams[allMatches[i][1]].getTeamName());
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Matchday "+(i+1)+" :  "+ allTeams[allMatches[i][0]].getTeamName() + " vs " + allTeams[allMatches[i][1]].getTeamName());
 			
 			int pick = outcomes[new Random().nextInt(outcomes.length)];
+			JLabel label1;
 			switch(pick)
 			{
 			case 0: // team 1 lost
@@ -29,7 +100,28 @@ public class MatchSimulation {
 				leagueTable[allMatches[i][1]][1]++;
 				leagueTable[allMatches[i][1]][2]++;
 				leagueTable[allMatches[i][1]][5] += 3;
-				System.out.println(allTeams[allMatches[i][1]].getTeamName() + " WON ");
+				System.out.println("                       " + allTeams[allMatches[i][1]].getTeamName() + " WON ");
+				
+				// set panel colors
+				label1 = new JLabel(allTeams[allMatches[i][0]].getTeamName());
+		    	c.gridx = 1;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.RIGHT);
+		    	label1.setBackground(Color.MAGENTA);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	label1 = new JLabel(allTeams[allMatches[i][1]].getTeamName());
+		    	c.gridx = 3;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.LEFT);
+		    	label1.setBackground(Color.GREEN);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	pane.revalidate();
+	    	    frame.revalidate();
+	    	    
 				break;
 				
 			case 1: // draw				
@@ -42,7 +134,27 @@ public class MatchSimulation {
 				leagueTable[allMatches[i][1]][1]++;
 				leagueTable[allMatches[i][1]][3]++;
 				leagueTable[allMatches[i][1]][5]++;
-				System.out.println(" DRAW ");
+				System.out.println("                       " + " DRAW ");
+				
+				// set panel colors
+				label1 = new JLabel(allTeams[allMatches[i][0]].getTeamName());
+		    	c.gridx = 1;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.RIGHT);
+		    	label1.setBackground(Color.YELLOW);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	label1 = new JLabel(allTeams[allMatches[i][1]].getTeamName());
+		    	c.gridx = 3;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.LEFT);
+		    	label1.setBackground(Color.YELLOW);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	pane.revalidate();
+	    	    frame.revalidate();
 				break;
 				
 			case 2: // team 1 win				
@@ -55,7 +167,27 @@ public class MatchSimulation {
 				leagueTable[allMatches[i][1]][1]++;
 				leagueTable[allMatches[i][1]][4]++;
 				
-				System.out.println(allTeams[allMatches[i][0]].getTeamName() + " WON ");
+				System.out.println("                       " + allTeams[allMatches[i][0]].getTeamName() + " WON ");
+				
+				// set panel colors
+				label1 = new JLabel(allTeams[allMatches[i][0]].getTeamName());
+		    	c.gridx = 1;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.RIGHT);
+		    	label1.setBackground(Color.GREEN);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	label1 = new JLabel(allTeams[allMatches[i][1]].getTeamName());
+		    	c.gridx = 3;
+		    	c.gridy = i;
+		    	label1.setHorizontalAlignment(SwingConstants.LEFT);
+		    	label1.setBackground(Color.MAGENTA);
+		    	label1.setOpaque(true);
+		    	pane.add(label1,c);
+		    	
+		    	pane.revalidate();
+	    	    frame.revalidate();
 				break;
 				
 			}
@@ -76,8 +208,17 @@ public class MatchSimulation {
 	
 	public int[][] leagueStanding(int [][] leagueTable) 
 	{
-		int[][] leagueStanding = leagueTable;
-		Arrays.sort(leagueStanding, Comparator.comparingInt(arr ->arr[5]));
+		int[][] leagueStandingReverse = leagueTable;
+		int[][] leagueStanding = new int[leagueTable.length][6];
+		Arrays.sort(leagueStandingReverse, Comparator.comparingInt(arr ->arr[5]));
+				
+		for(int i=0; i<leagueTable.length; i++)
+		{
+			for(int j=0; j<6; j++)
+			{
+				leagueStanding[(leagueTable.length-1-i)][j] = leagueStandingReverse[i][j];
+			}		
+		}
 		return leagueStanding;
 	}
 }
